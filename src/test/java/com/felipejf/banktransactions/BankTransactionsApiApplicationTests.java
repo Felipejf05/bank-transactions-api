3b170e107e6cb3d5f5dd62b08b2cb174ae430a32
FelipeJf05 <jorgefelipe.jf01@gmail.com>
2025-10-23 17:03:42 -0300

    Commit inicial com setup, gitignore e env

---
+++ src/test/java/com/felipejf/banktransactions/BankTransactionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipejf.banktransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
