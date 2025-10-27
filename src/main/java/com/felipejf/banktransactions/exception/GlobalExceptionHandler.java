618a8a53aeaaf13c2bb4d5c29d00bd3b1f70d6f5
FelipeJf05 <jorgefelipe.jf01@gmail.com>
2025-10-27 18:29:05 -0300

    Impl bank service

---
+++ src/main/java/com/felipejf/banktransactions/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.felipejf.banktransactions.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<String> handleAccountNotFoundException(AccountNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<String> handleBusinessException(BusinessException e){
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+}
